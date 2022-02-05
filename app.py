9ff0bc2a73ac5b7cb5c3ff4100dacc270bc79062
erma0x <35337869+erma0x@users.noreply.github.com>
2022-02-05 20:13:10 +0100

    update tickers

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 # test
 """
-1. 
-2. radiobutton con 2 dataset disponibili, ETHUSD e EOSBTC
-3. funzione che legge la risposta del radiobutton e a seconda di quello lancia un data_loader(): 
+DONE
+ radiobutton con 2 dataset disponibili, ETHUSD e EOSBTC
 
+TO DO 
+funzione che legge la risposta del radiobutton e a seconda di quello lancia un data_loader(): 
+
+
+optimization of the opening signal
+optimization of TP & SL
 """
 
 import streamlit as st
@@ -11,10 +16,21 @@ import pandas as pd
 
 description = """
 Using the HP filter to compose a strategy and test it. \n
-Optimization of the opening signal. \n
-Optimization of TP & SL \n
 """
 st.write(description)
-aviables_tickers = ('EOS-BTC 2020-2021 1 day ','ETH-USDT 2020-2021 1 minute ')
+aviables_tickers = ('EOS-USD | 2017 -> Feb 2022 1 day ','ETH-USD | 1 Jan 2020 - 1 Jan 2021 1 minute ')
 
-st.radio(label='Aviables Tickers ', options=aviables_tickers)
+
+choosen_ticker = st.radio(label='Aviables Tickers ', options=aviables_tickers)
+
+
+
+
+if  'EOS-BTC' in choosen_ticker:
+    st.write('EOS-BTC incoming')
+
+
+elif 'ETH-USDT':
+    st.write("You didn't select comedy.")
+else:
+    st.write("Sorry, we could't solve your request")
