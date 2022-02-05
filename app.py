39cf437ff9498d35073def866e348955650d66da
erma0x <35337869+erma0x@users.noreply.github.com>
2022-02-05 19:41:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ st.write(description)
 aviables_tickers = ('EOS-BTC','ETH-USDT')
 
 st.radio(label='aviables_tickers', options=aviables_tickers, index=0, format_func=special_internal_function, 
-         key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
+         key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
