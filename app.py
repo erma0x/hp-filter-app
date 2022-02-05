010889e4631fd3d5b353c7b20df596ffcfb276f8
erma0x <35337869+erma0x@users.noreply.github.com>
2022-02-05 19:33:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 
 st.write("hello world")
+
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
