b69c0065b49858443d4c63bb5cb634d8fdb51390
erma0x <35337869+erma0x@users.noreply.github.com>
2022-02-05 19:41:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 # test
 """
-# My first app
-Here's our first attempt at using data to create a table:
+1. 
+2. radiobutton con 2 dataset disponibili, ETHUSD e EOSBTC
+3. funzione che legge la risposta del radiobutton e a seconda di quello lancia un data_loader(): 
+
 """
 
 import streamlit as st
 import pandas as pd
 
-st.write("hello world")
+description = """
+Using the HP filter to compose a strategy and test it.
+Optimization of the opening signal.
+Optimization of TP & SL
+"""
+st.write(description)
+aviables_tickers = ('EOS-BTC','ETH-USDT')
 
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+st.radio(label='aviables_tickers', options=aviables_tickers, index=0, format_func=special_internal_function, 
+         key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
