894d5a597bc3cf5fb5a62d8d1d8253b2b31d534d
erma0x <35337869+erma0x@users.noreply.github.com>
2022-05-01 16:54:26 +0200

    reformat text

--- app.py
+++ app.py
@@ -8,25 +8,25 @@ from scipy import stats
 st.title('The Hodrick–Prescott filter for Fintech')
 
 description = """
-Seasonality decomposition in time series analysis with Hodrick–Prescott filter. \n
+Seasonality decomposition in time series analysis with Hodrick–Prescott filter.
 The Hodrick–Prescott filter is a mathematical tool used in macroeconomics, 
 especially in real business cycle theory, to remove the cyclical component 
 of a time series from raw data. It is used to obtain a smoothed-curve representation
 of a time series, one that is more sensitive to long-term than to short-term fluctuations.
 
-Hodrick Prescott Filter does Time series decomposition which involves separating \n 
-a time series into several distinct components(Cycle component and Trend Component). \n 
-And this filter looks like it can be applied to any time-series data especially with stock prices \n
-to understand the underlying trend and the cycle involved in it. \n
+Hodrick Prescott Filter does Time series decomposition which involves separating 
+a time series into several distinct components(Cycle component and Trend Component).  
+And this filter looks like it can be applied to any time-series data especially with stock prices 
+to understand the underlying trend and the cycle involved in it. 
 
-The HP filter is one of the most widely used tools in macroeconomic analysis.\n
-It tends to have favorable results if the noise is distributed normally,\n
-and when the analysis being conducted is historical.\n
-According to a paper published by economist and professor James Hamilton—which\n
-appears on the National Bureau of Economic Research website—there are several reasons why\n
-the HP filter should not be used. Hamilton first proposes that the filer produces outcomes\n
-that have no basis in the process of generating data. He also states that the values\n
-that are filtered at the sample's end are totally different from those in the middle. \n
+The HP filter is one of the most widely used tools in macroeconomic analysis.
+It tends to have favorable results if the noise is distributed normally,
+and when the analysis being conducted is historical.
+According to a paper published by economist and professor James Hamilton—which
+appears on the National Bureau of Economic Research website—there are several reasons why
+the HP filter should not be used. Hamilton first proposes that the filer produces outcomes
+that have no basis in the process of generating data. He also states that the values
+that are filtered at the sample's end are totally different from those in the middle. 
 
 """
 
@@ -63,7 +63,7 @@ def app():
         ## generate the data and plot it for an ideal normal curve
         st.write("Microsoft daily return mean ",df['daily_return'].mean())
         st.write("Microsoft daily return standard deviation ",df['daily_return'].std())
-        
+
     else:
         st.write("Sorry, we could't solve your request")
 
