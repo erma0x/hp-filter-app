737bcd2e9c70b6e3b0e87cc9068305983314df65
erma0x <35337869+erma0x@users.noreply.github.com>
2022-05-01 16:46:02 +0200

    update daily returns

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import streamlit as st
 import statsmodels.api as sm
+import numpy as np
+import scipy as sp
+from scipy import stats
 
-st.title('WebApp for Fintech World')
+st.title('The Hodrick–Prescott filter for Fintech')
 
 description = """
 Seasonality decomposition in time series analysis with Hodrick–Prescott filter. \n
@@ -10,7 +13,21 @@ The Hodrick–Prescott filter is a mathematical tool used in macroeconomics,
 especially in real business cycle theory, to remove the cyclical component 
 of a time series from raw data. It is used to obtain a smoothed-curve representation
 of a time series, one that is more sensitive to long-term than to short-term fluctuations.
-\n
+
+Hodrick Prescott Filter does Time series decomposition which involves separating \n 
+a time series into several distinct components(Cycle component and Trend Component). \n 
+And this filter looks like it can be applied to any time-series data especially with stock prices \n
+to understand the underlying trend and the cycle involved in it. \n
+
+The HP filter is one of the most widely used tools in macroeconomic analysis.\n
+It tends to have favorable results if the noise is distributed normally,\n
+and when the analysis being conducted is historical.\n
+According to a paper published by economist and professor James Hamilton—which\n
+appears on the National Bureau of Economic Research website—there are several reasons why\n
+the HP filter should not be used. Hamilton first proposes that the filer produces outcomes\n
+that have no basis in the process of generating data. He also states that the values\n
+that are filtered at the sample's end are totally different from those in the middle. \n
+
 """
 
 st.write(description)
@@ -42,6 +59,11 @@ def app():
         
         st.write("Microsoft daily return")
         st.line_chart(df['daily_return'])
+        
+        ## generate the data and plot it for an ideal normal curve
+        ## y-axis as the gaussian
+        y_data = stats.norm.pdf(daily_return, daily_return.mean(), daily_return.sd())
+        st.line_chart(daily_return, y_data)
 
     else:
         st.write("Sorry, we could't solve your request")
