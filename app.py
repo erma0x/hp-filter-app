dc8ebf4d876a4122609fecf780c743d35513fd47
erma0x <35337869+erma0x@users.noreply.github.com>
2022-05-01 16:26:39 +0200

    test charting

--- app.py
+++ app.py
@@ -26,28 +26,36 @@ of a time series from raw data. It is used to obtain a smoothed-curve representa
 of a time series, one that is more sensitive to long-term than to short-term fluctuations.
 \n
 """
+
 st.write(description)
+
 datasets_available  = ("Microsoft MSFT - adjusted close price - daily - 2017-01-03 to 2021-09-10",
-"(Test) Apple APPL")
+                       "(Test) Apple APPL")
 
 choosen_ticker = st.radio(label='Datesets avaiable:', options=datasets_available)
 
 def app():
     if  'Microsoft' in choosen_ticker:
-        st.write('Microsoft incoming')
+        
         df = pd.read_csv('MSFT.csv')
+        
+        st.write('Microsoft (MSFT) dataset (first 5 rows)')
         st.write(df.head())
+        
         st.write('Microsoft daily adjusted close price ')
         st.line_chart(df['Adj Close'])
 
         # PRICE DECOMPOSITION
         data_cycle, data_trend = sm.tsa.filters.hpfilter(df['Adj Close'])
+        
         st.write("Mircosoft data")
-        st.line_chart(data_cycle)
+        st.area_chart(data_cycle)
+        
         st.write("Microsoft data trend with HP filter")
         st.line_chart(data_trend)
+        
         st.write("Microsoft daily return")
-        st.line_chart(df['daily_return'])
+        st.area_chart(df['daily_return'])
 
     else:
         st.write("Sorry, we could't solve your request")
