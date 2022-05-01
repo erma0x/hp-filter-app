0ad1f38bfebc4b224974ac2e173cafcb599d4784
erma0x <35337869+erma0x@users.noreply.github.com>
2022-05-01 16:37:25 +0200

    update app

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-# test
-"""
-DONE
- radiobutton con 2 dataset disponibili, ETHUSD e EOSBTC
-funzione che legge la risposta del radiobutton e a seconda di quello lancia un data_loader(): 
-
-TO DO 
-applica le bollinger bands alla decomposizione, le bollinger hanno una finestra per essere calcolate
-prova ad entrare al rialzo quando sta sotto < % di probabilita' della bollinger bands
-
-
-optimization of the opening signal
-optimization of TP & SL
-"""
-import streamlit as st
-
 import pandas as pd
+import streamlit as st
 import statsmodels.api as sm
 
+st.title('WebApp with Streamlit for Fintech World')
+
 description = """
-Seasonality decomposition in time series analysis with Hodrick–Prescott filter
-\n
+Seasonality decomposition in time series analysis with Hodrick–Prescott filter. \n
 The Hodrick–Prescott filter is a mathematical tool used in macroeconomics, 
 especially in real business cycle theory, to remove the cyclical component 
 of a time series from raw data. It is used to obtain a smoothed-curve representation
@@ -49,13 +35,13 @@ def app():
         data_cycle, data_trend = sm.tsa.filters.hpfilter(df['Adj Close'])
         
         st.write("Mircosoft data")
-        st.area_chart(data_cycle)
+        st.line_chart(data_cycle)
         
         st.write("Microsoft data trend with HP filter")
         st.line_chart(data_trend)
         
         st.write("Microsoft daily return")
-        st.area_chart(df['daily_return'])
+        st.bar_chart(df['daily_return'])
 
     else:
         st.write("Sorry, we could't solve your request")
